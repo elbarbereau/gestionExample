4fd4f528903c1bda51db59b5ce192ae0b3c5c475
elbarbereau <98040453+elbarbereau@users.noreply.github.com>
2022-02-07 11:41:59 +0100

    intial state version spring

--- gestionExample2/src/main/java/fr/formation/inti/service/EmployeeService.java
+++ gestionExample2/src/main/java/fr/formation/inti/service/EmployeeService.java
@@ -1,5 +1,48 @@
 package fr.formation.inti.service;
 
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.formation.inti.dao.IEmployeeDao;
+import fr.formation.inti.entity.Employee;
+
+@Service
 public class EmployeeService implements IEmployeeService{
+	
+	@SuppressWarnings("unused")
+	private static final Log log = LogFactory.getLog(EmployeeService.class);
+	
+	@Autowired
+	private IEmployeeDao dao;
+
+	@Override
+	public Integer save(Employee employee) {
+		return dao.save(employee);
+	}
+
+	@Override
+	public void update(Employee employee) {
+		dao.update(employee);
+	}
+
+	@Override
+	public void delete(Employee employee) {
+		dao.delete(employee);
+	}
+
+	@Override
+	public Employee findById(Integer id) {
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		List<Employee> list = dao.findAll();
+		return list;
+	}
 
 }
--- gestionExample2/src/main/java/fr/formation/inti/service/IEmployeeService.java
+++ gestionExample2/src/main/java/fr/formation/inti/service/IEmployeeService.java
@@ -1,5 +1,19 @@
 package fr.formation.inti.service;
 
+import java.util.List;
+
+import fr.formation.inti.entity.Employee;
+
 public interface IEmployeeService {
+
+	Integer save(Employee employee);
+	
+	void update(Employee employee);
+	
+	void delete(Employee employee);
+	
+	Employee findById(Integer id);
+	
+	List<Employee> findAll();
 
 }
